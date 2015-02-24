c050e867fed0d4ce1afd46b82d5191c9794bd3d7
itaip <itaip@tikalk.com>
2015-02-24 12:25:16 +0200

    improve attack

--- src/main/java/tikal/controller/CheckinController.java
+++ src/main/java/tikal/controller/CheckinController.java
@@ -1,23 +1,21 @@
 package tikal.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import tikal.dos.AtomicCounter;
 import tikal.model.Checkin;
 
 
 @RestController
 public class CheckinController {
+    private AtomicCounter counter = new AtomicCounter();
 	
 	@RequestMapping(value="/checkin",method=RequestMethod.POST)
 	public Object checkin(@RequestBody final Checkin checkin) {
 		checkin.setTimestamp(System.currentTimeMillis());
-		System.out.println(checkin);
+		System.out.println(" - Num of Request: " + counter.increment());
 		return true;
 	}
 }
--- src/main/java/tikal/controller/DoSController.java
+++ src/main/java/tikal/controller/DoSController.java
@@ -1,21 +1,18 @@
 package tikal.controller;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import tikal.dos.AtomicCounter;
 import tikal.dos.Attack;
 import tikal.dos.BasicHttpAttack;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 public class DoSController {
@@ -24,14 +21,14 @@ public class DoSController {
 
 	ExecutorService execService = Executors.newCachedThreadPool();
 	
-	@RequestMapping(value="/attack/{targetIP}/{threads}",method=RequestMethod.POST)
+	@RequestMapping(value="/attack/{targetIP}/{threads}",method=RequestMethod.GET)
 	public Object start(@PathVariable final int threads, @PathVariable final String targetIP){
 		System.out.println(threads);
 		System.out.println(targetIP);
-		
-		try {
+        AtomicCounter atomicCounter = new AtomicCounter();
+        try {
 			for (int i = 0; i < threads; i++) {
-				Attack attack = new BasicHttpAttack();
+				Attack attack = new BasicHttpAttack(atomicCounter);
 				attack.setUrl("http://"+targetIP+":8080/checkin");
 				ongoingAttacks.add(attack);
 				execService.execute(attack);
@@ -45,11 +42,16 @@ public class DoSController {
 		return true;
 	}
 	
-	@RequestMapping(value="/attack",method=RequestMethod.DELETE)
+	@RequestMapping(value="/stop",method=RequestMethod.GET)
 	public Object stop(){
 		for (Attack attack : ongoingAttacks) {
 			attack.stop();
 		}
 		return true;
 	}
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test() {
+        return "Baruch Jamili " + new Date();
+    }
 }
