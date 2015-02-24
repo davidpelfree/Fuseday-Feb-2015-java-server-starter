a9353a0e317f13e0bee630317144060c6148f259
David Peleg <david@tikalk.com>
2015-02-24 14:44:12 +0200

    aaa

--- src/main/java/tikal/FuseDayStarterApplication.java
+++ src/main/java/tikal/FuseDayStarterApplication.java
@@ -2,6 +2,8 @@ package tikal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootApplication
 public class FuseDayStarterApplication {
@@ -9,4 +11,26 @@ public class FuseDayStarterApplication {
     public static void main(String[] args) {
         SpringApplication.run(FuseDayStarterApplication.class, args);
     }
+
+//    public static void main(String[] args) {
+//        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+//        ctx.register(RedisConfig.class);
+//        ctx.refresh();
+//
+//        StringRedisTemplate stringRedisTemplate  = ctx.getBean(StringRedisTemplate.class);
+//
+//        // Using set to set value
+//        stringRedisTemplate.opsForValue().set("R", "Ram");
+//        stringRedisTemplate.opsForValue().set("S", "Shyam");
+//
+//        //Fetch values from set
+//        System.out.println(stringRedisTemplate.opsForValue().get("R"));
+//        System.out.println(stringRedisTemplate.opsForValue().get("S"));
+//
+//        //Using Hash Operation
+//        String mohan = "Mohan";
+//        stringRedisTemplate.opsForHash().put("M", String.valueOf(mohan.hashCode()),mohan);
+//        System.out.println(stringRedisTemplate.opsForHash().get("M", String.valueOf(mohan.hashCode())));
+//    }
+
 }
--- src/main/java/tikal/controller/CheckinController.java
+++ src/main/java/tikal/controller/CheckinController.java
@@ -3,6 +3,10 @@ package tikal.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,11 +17,36 @@ import tikal.model.Checkin;
 
 @RestController
 public class CheckinController {
+
+//    @Autowired
+//    StringRedisTemplate stringRedisTemplate;
+//
+//    @Autowired
+//    RedisTemplate<String,Long> longRedisTemplate;
 	
 	@RequestMapping(value="/checkin",method=RequestMethod.POST)
 	public Object checkin(@RequestBody final Checkin checkin) {
-		checkin.setTimestamp(System.currentTimeMillis());
+
+        long now = System.currentTimeMillis();
+        checkin.setTimestamp(now);
 		System.out.println(checkin);
+
+        long nowAsSec = now % 1000;
+
+        long rpsKey = nowAsSec;
+//        longRedisTemplate.<Long,Long>opsForHash().increment("meta", rpsKey, 1L);
+
+
+//        // Using set to set value
+//        String key = "user:" + checkin.getUserId();
+//        String value = checkin.getLatitude() + ":" + checkin.getLongitude() + ":" + checkin.getTimestamp();
+//        stringRedisTemplate.opsForValue().set(key, value);
+//        stringRedisTemplate.opsForValue().set("S", "Shyam");
+//
+//        //Fetch values from set
+//        System.out.println(stringRedisTemplate.opsForValue().get("R"));
+//        System.out.println(stringRedisTemplate.opsForValue().get("S"));
+
 		return true;
 	}
 }
