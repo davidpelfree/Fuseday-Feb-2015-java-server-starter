af3b74e46141616483e4e216f60010d9620a568c
David Peleg <david@tikalk.com>
2015-02-24 15:06:31 +0200

    Now boots ok

--- src/main/java/tikal/controller/CheckinController.java
+++ src/main/java/tikal/controller/CheckinController.java
@@ -3,6 +3,9 @@ package tikal.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,11 +18,11 @@ import tikal.model.Checkin;
 public class CheckinController {
     private AtomicCounter counter = new AtomicCounter();
 
-//    @Autowired
-//    StringRedisTemplate stringRedisTemplate;
-//
-//    @Autowired
-//    RedisTemplate<String,Long> longRedisTemplate;
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    RedisTemplate<String,Long> longRedisTemplate;
 	
 	@RequestMapping(value="/checkin",method=RequestMethod.POST)
 	public Object checkin(@RequestBody final Checkin checkin) {
@@ -33,7 +36,7 @@ public class CheckinController {
         long nowAsSec = now % 1000;
 
         long rpsKey = nowAsSec;
-//        longRedisTemplate.<Long,Long>opsForHash().increment("meta", rpsKey, 1L);
+        longRedisTemplate.<Long,Long>opsForHash().increment("meta", rpsKey, 1L);
 
 
 //        // Using set to set value
